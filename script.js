470f14a89ae51bd3fdec86ea22c34d4808e6bba0
julianycia.m@gmail.com <julianycia.m@gmail.com>
2024-07-03 13:34:40 -0300

    subimos proyecto alura

---
+++ script.js
@@ -0,0 +1,52 @@
+function encriptar() {
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let noencontro = document.getElementById("noencontro");
+  let regex = /^[a-z]+$/;
+  let textoCifrado = texto
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
+
+  if (texto.length != 0 && regex.test(texto)) {
+    document.getElementById("texto").value = textoCifrado;
+    tituloMensaje.textContent = "Texto encriptado con éxito";
+    parrafo.textContent = "";
+    noencontro.src = "./image/robot.png";
+  } else {
+    noencontro.src = "./image/noencontro.png";
+    tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+    parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+    alert("Recuerda ingresar un texto válido");
+  }
+}
+
+function desencriptar() {
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let noencontro = document.getElementById("noencontro");
+  let regex = /^[a-z]+$/;
+
+  let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+
+  if (texto.length != 0 && regex.test(texto)) {
+    document.getElementById("texto").value = textoCifrado;
+    tituloMensaje.textContent = "Texto desencriptado con éxito";
+    parrafo.textContent = "";
+    noencontro.src = "./image/ia.png";
+  } else {
+    noencontro.src = "./image/noencontro.png";
+    tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+    parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+    alert("Recuerda ingresar un texto válido");
+  }
+}
